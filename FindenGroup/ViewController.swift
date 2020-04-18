2e60170f2824fb4b4e6d96af4186e8e97383edc7
Songeun Kim <faborange610@gmail.com>
2020-04-19 05:05:56 +0900

    Initial Commit

---
+++ FindenGroup/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FindenGroup
+//
+//  Created by 김송은 on 4/19/20.
+//  Copyright © 2020 faborange610@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
